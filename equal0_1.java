80557007bd8046d939831e1465558550e9a038c5
Rahul-varma-682 <133089009+Rahul-varma-682@users.noreply.github.com>
2024-04-08 18:04:12 +0530

    Add files via upload

---
+++ equal0_1.java
@@ -0,0 +1,18 @@
+public class equal0_1 {
+    
+        public static void main(String[] args) {
+            int number1 = 7;
+            int number2 = 6;
+            
+            // Check if the two numbers are equal
+            String result;
+            if (number1 == number2) {
+                result = "equal";
+            } else {
+                result = "not equal";
+            }
+            
+            System.out.println("number1 and number2 values are " + result);
+        }
+    
+}
